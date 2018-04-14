be08013c6fcee5af4f1499f1f1a55ae7a3bfc375
hungluong249 <hung.luong@matocreative.vn>
2018-04-14 11:35:32 +0700

    fix 060418

--- application/views/proresident.php
+++ application/views/proresident.php
@@ -1,186 +1,186 @@
 <div class="container-fluid">
-<!-- InstanceBeginEditable name="content" -->
-<div class="col-md-4 col-sm-4 col-xs-12 content_left">
-    <h4 class="title"><?php echo $this->lang->line('Tower'); ?></h4>
-    <div class="text fadeinUp">
-        <p class="paragraph"><?php echo $this->lang->line('Tower_1'); ?></p>
-        <p class="paragraph"><?php echo $this->lang->line('Tower_2'); ?></p>
-        <a href="#" data-toggle="modal" data-target="#map_bg"><?php echo $this->lang->line('Tower_view_more'); ?></a>
-    </div>
-</div>
-<div class="col-md-8 col-sm-8 col-xs-12 content_right resident">
-    <div class="room_select" id="room_select">
-        <div class="room" id="room_a">
-            <div class="wrapper">
-                <img src="<?php echo site_url('assets/public/img/nt_001.jpg'); ?>" alt="">
-                <div class="inner">
-                    <h4 class="title"><?php echo $this->lang->line('Tower_Apartment'); ?> A <br> 113,6m&sup2;</h4>
-                </div>
-            </div>
-        </div>
-        <div class="room" id="room_b">
-            <div class="wrapper">
-                <img src="<?php echo site_url('assets/public/img/nt_003.jpg'); ?>" alt="">
-                <div class="inner">
-                    <h4 class="title"><?php echo $this->lang->line('Tower_Apartment'); ?> B1 <br> 92,2m&sup2;</h4>
-                </div>
-            </div>
-        </div>
-        <div class="room" id="room_c">
-            <div class="wrapper">
-                <img src="<?php echo site_url('assets/public/img/nt_004.jpg'); ?>" alt="">
-                <div class="inner">
-                    <h4 class="title"><?php echo $this->lang->line('Tower_Apartment'); ?> B2 <br> 94,1m&sup2;</h4>
-                </div>
-            </div>
-        </div>
-        <div class="room" id="room_d">
-            <div class="wrapper">
-                <img src="<?php echo site_url('assets/public/img/nt_003.jpg'); ?>" alt="">
-                <div class="inner">
-                    <h4 class="title"><?php echo $this->lang->line('Tower_Apartment'); ?> C <br> 151,5m&sup2;</h4>
-                </div>
-            </div>
-        </div>
-        
-    </div>
-    <div id="re_a" class="carousel slide" data-ride="carousel">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-      
-    
-      <!-- Wrapper for slides -->
-      <div class="carousel-inner" role="listbox">
-        <div class="item active">
-          <img src="<?php echo site_url('assets/public/img/nt_001.jpg'); ?>" alt="...">
-          
-        </div>
-        <div class="item">
-          <img src="<?php echo site_url('assets/public/img/nt_002.jpg'); ?>" alt="...">
-          
-        </div>
-        <div class="item">
-          <img src="<?php echo site_url('assets/public/img/nt_003.jpg'); ?>" alt="...">
-          
-        </div>
-      </div>
-    
-
-      <a class="left carousel-control" href="#re_a" role="button" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-      </a>
-    </div> 
-    
-    <div id="re_b" class="carousel slide" data-ride="carousel">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-      
-    
-      <!-- Wrapper for slides -->
-      <div class="carousel-inner" role="listbox">
-        <div class="item active">
-          <img src="<?php echo site_url('assets/public/img/92.jpg'); ?>" alt="...">
-          
-        </div>
-        <div class="item">
-          <img src="<?php echo site_url('assets/public/img/nt_001.jpg'); ?>" alt="...">
-          
-        </div>
-        <div class="item">
-          <img src="<?php echo site_url('assets/public/img/nt_003.jpg'); ?>" alt="...">
-          
-        </div>
-      </div>
-    
-      <!-- Controls 
-      <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-      </a>-->
-    </div> 
-    
-    <div id="re_c" class="carousel slide" data-ride="carousel">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-      
-    
-      <!-- Wrapper for slides -->
-      <div class="carousel-inner" role="listbox">
-        <div class="item active">
-          <img src="<?php echo site_url('assets/public/img/94.jpg'); ?>" alt="...">
-          
-        </div>
-        <div class="item">
-          <img src="<?php echo site_url('assets/public/img/nt_002.jpg'); ?>" alt="...">
-          
-        </div>
-        <div class="item">
-          <img src="<?php echo site_url('assets/public/img/nt_001.jpg'); ?>" alt="...">
-          
-        </div>
-      </div>
-    
-      <!-- Controls 
-      <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-      </a>-->
-    </div>
-    <div id="re_d" class="carousel slide" data-ride="carousel">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-      
-    
-      <!-- Wrapper for slides -->
-      <div class="carousel-inner" role="listbox">
-        <div class="item active">
-          <img src="<?php echo site_url('assets/public/img/151.jpg'); ?>" alt="...">
-          
-        </div>
-        <div class="item">
-          <img src="<?php echo site_url('assets/public/img/nt_002.jpg'); ?>" alt="...">
-          
-        </div>
-        <div class="item">
-          <img src="<?php echo site_url('assets/public/img/nt_001.jpg'); ?>" alt="...">
-          
-        </div>
-      </div>
-    
-      <!-- Controls 
-      <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-      </a>-->
-    </div>        
-</div>
-
-
-<div class="modal fade" id="map_bg" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-	<div class="modal-dialog modal-lg" role="document">
-		<div class="modal-content">
-
-			<div class="modal-body">
-				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-				<img src="<?php echo site_url('assets/public/img/map.jpg'); ?>" alt="">
+	<!-- InstanceBeginEditable name="content" -->
+	<div class="col-md-4 col-sm-4 col-xs-12 content_left">
+		<h4 class="title"><?php echo $this->lang->line('Tower'); ?></h4>
+		<div class="text fadeinUp">
+			<p class="paragraph"><?php echo $this->lang->line('Tower_1'); ?></p>
+			<p class="paragraph"><?php echo $this->lang->line('Tower_2'); ?></p>
+			<a href="#" data-toggle="modal" data-target="#map_bg"><?php echo $this->lang->line('Tower_view_more'); ?></a>
+		</div>
+	</div>
+	<div class="col-md-8 col-sm-8 col-xs-12 content_right resident">
+		<div class="room_select" id="room_select">
+			<div class="room" id="room_a">
+				<div class="wrapper">
+					<img src="<?php echo site_url('assets/public/img/nt_001.jpg'); ?>" alt="">
+					<div class="inner">
+						<h4 class="title"><?php echo $this->lang->line('Tower_Apartment'); ?> A <br> 113,6m&sup2;</h4>
+					</div>
+				</div>
+			</div>
+			<div class="room" id="room_b">
+				<div class="wrapper">
+					<img src="<?php echo site_url('assets/public/img/nt_003.jpg'); ?>" alt="">
+					<div class="inner">
+						<h4 class="title"><?php echo $this->lang->line('Tower_Apartment'); ?> B1 <br> 92,2m&sup2;</h4>
+					</div>
+				</div>
+			</div>
+			<div class="room" id="room_c">
+				<div class="wrapper">
+					<img src="<?php echo site_url('assets/public/img/nt_004.jpg'); ?>" alt="">
+					<div class="inner">
+						<h4 class="title"><?php echo $this->lang->line('Tower_Apartment'); ?> B2 <br> 94,1m&sup2;</h4>
+					</div>
+				</div>
+			</div>
+			<div class="room" id="room_d">
+				<div class="wrapper">
+					<img src="<?php echo site_url('assets/public/img/nt_003.jpg'); ?>" alt="">
+					<div class="inner">
+						<h4 class="title"><?php echo $this->lang->line('Tower_Apartment'); ?> C <br> 151,5m&sup2;</h4>
+					</div>
+				</div>
 			</div>
 
 		</div>
-	</div>
-</div>
+		<div id="re_a" class="carousel slide" data-ride="carousel">
+			<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
 
-<!-- InstanceEndEditable -->
+
+			<!-- Wrapper for slides -->
+			<div class="carousel-inner" role="listbox">
+				<div class="item active">
+					<img src="<?php echo site_url('assets/public/img/nt_001.jpg'); ?>" alt="...">
+
+				</div>
+				<div class="item">
+					<img src="<?php echo site_url('assets/public/img/nt_002.jpg'); ?>" alt="...">
+
+				</div>
+				<div class="item">
+					<img src="<?php echo site_url('assets/public/img/nt_003.jpg'); ?>" alt="...">
+
+				</div>
+			</div>
+
+
+			<a class="left carousel-control" href="#re_a" role="button" data-slide="prev">
+				<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+				<span class="sr-only">Previous</span>
+			</a>
+			<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+				<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+				<span class="sr-only">Next</span>
+			</a>
+		</div>
+
+		<div id="re_b" class="carousel slide" data-ride="carousel">
+			<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+
+
+			<!-- Wrapper for slides -->
+			<div class="carousel-inner" role="listbox">
+				<div class="item active">
+					<img src="<?php echo site_url('assets/public/img/92.jpg'); ?>" alt="...">
+
+				</div>
+				<div class="item">
+					<img src="<?php echo site_url('assets/public/img/nt_001.jpg'); ?>" alt="...">
+
+				</div>
+				<div class="item">
+					<img src="<?php echo site_url('assets/public/img/nt_003.jpg'); ?>" alt="...">
+
+				</div>
+			</div>
+
+			<!-- Controls
+            <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+              <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+              <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>-->
+		</div>
+
+		<div id="re_c" class="carousel slide" data-ride="carousel">
+			<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+
+
+			<!-- Wrapper for slides -->
+			<div class="carousel-inner" role="listbox">
+				<div class="item active">
+					<img src="<?php echo site_url('assets/public/img/94.jpg'); ?>" alt="...">
+
+				</div>
+				<div class="item">
+					<img src="<?php echo site_url('assets/public/img/nt_002.jpg'); ?>" alt="...">
+
+				</div>
+				<div class="item">
+					<img src="<?php echo site_url('assets/public/img/nt_001.jpg'); ?>" alt="...">
+
+				</div>
+			</div>
+
+			<!-- Controls
+            <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+              <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+              <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>-->
+		</div>
+		<div id="re_d" class="carousel slide" data-ride="carousel">
+			<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+
+
+			<!-- Wrapper for slides -->
+			<div class="carousel-inner" role="listbox">
+				<div class="item active">
+					<img src="<?php echo site_url('assets/public/img/151.jpg'); ?>" alt="...">
+
+				</div>
+				<div class="item">
+					<img src="<?php echo site_url('assets/public/img/nt_002.jpg'); ?>" alt="...">
+
+				</div>
+				<div class="item">
+					<img src="<?php echo site_url('assets/public/img/nt_001.jpg'); ?>" alt="...">
+
+				</div>
+			</div>
+
+			<!-- Controls
+            <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+              <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+              <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>-->
+		</div>
+	</div>
+
+
+	<div class="modal fade" id="map_bg" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+		<div class="modal-dialog modal-lg" role="document">
+			<div class="modal-content">
+
+				<div class="modal-body">
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+					<img src="<?php echo site_url('assets/public/img/map.jpg'); ?>" alt="">
+				</div>
+
+			</div>
+		</div>
+	</div>
+
+	<!-- InstanceEndEditable -->
 </div>
